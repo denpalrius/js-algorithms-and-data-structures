05f46a5ad7c20ece9b82c8fda747573022fef01b
Dennis Riungu <denpalrius@gmail.com>
2021-04-07 21:00:57 +0300

    Add test data

--- leap/graph_undirected_direct_path.js
+++ leap/graph_undirected_direct_path.js
@@ -23,6 +23,7 @@ function solution(N, A, B) {
     const nextEdges = adjacencyList[i];
 
     if (!nextEdges || nextEdges.indexOf(i + 1) < 0) {
+      console.log(i + ":", nextEdges);
       return false;
     }
   }
@@ -30,7 +31,14 @@ function solution(N, A, B) {
   return true;
 }
 
-console.log({
-  expected: true,
-  got: solution(4, [1, 2, 4, 4, 3], [2, 3, 1, 3, 1]),
-});
+const testDataSet = [
+  { N: 4, A: [1, 2, 4, 4, 3], B: [2, 3, 1, 3, 1], expectedRes: true },
+  { N: 4, A: [1, 2, 1, 3], B: [2, 4, 3, 4], expectedRes: false },
+  { N: 6, A: [2, 4, 5, 3], B: [3, 5, 6, 4], expectedRes: false },
+  { N: 3, A: [1, 3], B: [2, 2], expectedRes: true },
+];
+
+for (data of testDataSet) {
+  const results = solution(data.N, data.A, data.B);
+  console.log({ expected: data.expectedRes, got: results });
+}
